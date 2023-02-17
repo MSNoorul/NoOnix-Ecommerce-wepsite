8864193c71940874dbb6f6df26d0af7678de7a14
noorul <noorulameen544@gamil.com>
2023-02-17 09:56:27 -0500

    some

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import data from '../assets/banner_img.json'
 function Banner() {
     return ( 
-        <div className='  container h-[70vh] mx-auto mb-11 relative'>
+        <div className='  container h-[70vh] px-2 mx-auto mb-11 relative'>
             <div className=' absolute w-full h-full flex flex-col justify-center items-center'>
                 <p className=' text-slate-400 font-semibold text-2xl tracking-wider mb-1'>Best Solo</p>
                 <p className=' text-slate-200 font-semibold text-3xl tracking-wider capitalize mb-1'>wireless</p>
-                <p className=' text-slate-100 font-bold text-7xl tracking-wider uppercase mb-4'>headphone</p>
+                <p className=' text-slate-100 font-bold text-5xl sm:text-7xl tracking-wider uppercase mb-4'>headphone</p>
                 <button className=" bg-slate-100 rounded-[25px] px-3 py-2 font-semibold text-slate-600"> shop now</button>
             </div>
            <img className=' w-full h-full rounded-xl  ' src="https://img.freepik.com/free-photo/music-arrangement-with-black-headphones-with-copy-space_23-2148785721.jpg?size=626&ext=jpg&ga=GA1.2.1111501466.1676095350&semt=ais" alt="img"  />
--- src/components/Notify.jsx
+++ src/components/Notify.jsx
@@ -2,7 +2,7 @@ function Notify({title}) {
     return ( 
     <div className=" Notify fixed z-10 bg-white left-1/2 top-[-60px] translate-x-[-50%] shadow-md
     py-3 px-6 rounded-[30px] border border-slate-200 w-fit flex gap-4 ">
-       <p className=" p-1 bg-green-600 text-white rounded-[50%] w-fit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+       <p className=" p-1 bg-green-600 text-white rounded-[50%] w-fit h-fit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
   <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
 </svg>
   </p><p className=" capitalize font-medium text-slate-700">item add to card</p>
