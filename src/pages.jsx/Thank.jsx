817595be058392275a230750da50d983f3bbb9c4
noorul <noorulameen544@gamil.com>
2023-02-18 01:17:50 -0500

    second commit

--- src/pages.jsx/Thank.jsx
+++ src/pages.jsx/Thank.jsx
@@ -1,12 +1,37 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { runconfetti } from "../utility/confitte";
+// import { runconfetti,interval } from "../utility/confitte";
+import confetti from 'canvas-confetti';
 
 function Thank() {
-  useEffect(() => {
-    runconfetti()
+
+  useEffect(() => {   
+  var duration = 5*1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 150, ticks: 60, zIndex: 0 };
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  var interval = setInterval(function() {
+    var timeLeft = animationEnd - Date.now();
+  
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+  
+  
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0, 1), y: Math.random() } }));
+  //   confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
+  
+   return () => {clearInterval(interval)}
   }, [])
   
+
   return (
     <section className=" thank flex h-[100vh] justify-center items-center p-5
         sm:p-10 lg:p-20">
